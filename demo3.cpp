da798f0dd5bb7bf3174ce50b7cfe84da53495ed8
leikang123 <1013626230@qq.com>
2021-09-08 16:56:04 +0800

    demo

---
+++ demo3.cpp
@@ -0,0 +1,23 @@
+ 
+#include <iostream>
+using namespace std;
+ 
+// main() 是程序开始执行的地方
+// 声明变量 
+extern int alb, c;
+extern int d;
+extern float f;
+ 
+int main()
+{
+// 变量定义
+int a ,b, c;
+int d;
+float f;
+a=10,b=13,c=20;
+d = a+b+c;
+cout << d << endl ;
+f = 7.0 /3.0;
+cout << f << endl;
+      return 0;
+}
